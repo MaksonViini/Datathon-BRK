ba2eb1140ce89329860f2dd80c599b3265eed0f0
maksonviini <maksonvinicio7@gmail.com>
2021-11-26 13:23:15 -0300

    alterandos setup.sh

--- App/app.py
+++ App/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(layout='wide')
